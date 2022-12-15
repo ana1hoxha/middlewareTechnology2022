c2327ada40d51c4d9c6f9a402d4d502451e18f1a
ana1hoxha <ana1hoxha@gmail.com>
2022-12-15 19:09:00 +0100

    adding frontend Demo

---
+++ frontendDemo/src/test/java/com/example/frontendTest/FrontendDemoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frontendTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendDemoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
